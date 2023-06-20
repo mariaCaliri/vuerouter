d6ab64e0df88f1da5aad31591221075885119266
CaliriMaria <maria.caliri@tecnasoft.it>
2023-06-20 17:05:15 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+const app = createApp(App)
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: App },
+        { path: '/teams', component: TeamsList },
+        { path: '/users', component: UsersList},
+        { path: '/teams/:teamId', component: TeamMembers}
+    ]
+});
+
+app.use(router);
+
+
+app.mount('#app');
